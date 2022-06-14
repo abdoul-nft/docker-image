1d1f4fb29731b8e9c3a556627a6994c9e1f960e5
abdoul-nft <abdoul.nft@gmail.com>
2022-06-14 16:12:33 +0200

    wip

--- app.js
+++ app.js
@@ -8,8 +8,30 @@ const PORT = 8080;
 // App
 const app = express();
 app.get('/', (req, res) => {
-  res.send('Hello World');
+  res.send('Hello World!');
 });
+
+
+app.get('/contact', (req, res) => {
+  res.send('Contact page');
+});
+
+
+app.get('/others', (req, res) => {
+  res.send('Others page');
+});
+
+
+app.get('/test', (req, res) => {
+  res.send('Test page');
+});
+
+
+app.get('/api', (req, res) => {
+  res.json({ 'response': "Hello API" });
+});
+
+
 
 app.listen(PORT, () => {
     console.log({
